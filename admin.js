a1fe1d82fbd199a5c7b7290fc43456f1d95bcbb4
Paulo Henrique <phvcandido@gmail.com>
2021-05-12 15:29:57 -0300

    Adicionando autenticação

--- admin.js
+++ admin.js
@@ -1,6 +1,7 @@
 const api = require('./routes/api');
 const env = require('dotenv').config();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 // const { admin, adminBroOptions } = require('./admin');
 
 const express = require("express");
@@ -11,15 +12,76 @@ const AdminBroExpress = require('@admin-bro/express')
 const AdminBroMongoose = require('@admin-bro/mongoose')
 
 const Project = require('./models/Project');
+const User = require('./models/User');
 
 AdminBro.registerAdapter(AdminBroMongoose);
 
 const adminBroOptions = new AdminBro({
-  resources: [Project],
+  resources: [
+    {
+      resource: Project, options: {
+        properties: {
+          description: { type: 'richtext' },
+          created_at: {
+            isVisible: { edit: false, list: true, show: true, filter: true }
+          }
+        }
+      }
+    },
+    {
+      resource: User,
+      options: {
+        properties: {
+          encryptedPassword: {
+            isVisible: false,
+          },
+          password: {
+            type: 'string',
+            isVisible: {
+              list: false, edit: true, filter: false, show: false,
+            },
+          },
+        },
+        actions: {
+          new: {
+            before: async (request) => {
+              if (request.payload.password) {
+                request.payload = {
+                  ...request.payload,
+                  encryptedPassword: await bcrypt.hash(request.payload.password, 10),
+                  password: undefined,
+                }
+              }
+              return request
+            },
+          }
+        }
+      }
+    }
+  ],
+  locale: {
+    translations: {
+      labels: {
+        Project: 'Projetos Desenvolvidos'
+      }
+    }
+  },
   rootPath: '/admin'
 });
 
-const admin = AdminBroExpress.buildRouter(adminBroOptions);
+const admin = AdminBroExpress.buildAuthenticatedRouter(adminBroOptions, {
+  authenticate: async (email, password) => {
+    const user = await User.findOne({ email })
+    if (user) {
+      const matched = await bcrypt.compare(password, user.encryptedPassword)
+      if (matched) {
+        return user
+      }
+    }
+    return false
+  },
+  cookiePassword: 'some-secret-password-used-to-secure-cookie',
+});
 
 
 server.use(adminBroOptions.options.rootPath, admin)
